344b44305897dfa177f27fa27a1fd5777e4d1b1e
C0A23056 <c0a23056d1@edu.teu.ac.jp>
2024-07-15 12:37:28 +0900

    スタート画面切り替え完成

--- Dreamer.py
+++ Dreamer.py
@@ -9,8 +9,8 @@ import pygame
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-WIDTH = 1100
-HEIGHT = 650
+WIDTH = 1600
+HEIGHT = 950
 
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
@@ -25,87 +25,74 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
-class Allen(pg.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pg.transform.rotozoom(pg.image.load("fig/スクリーンショット 2024-07-09 145858.png"), 0, 0.9)
-        self.rect = self.image.get_rect()
-        self.rect.center = 300, 200
-        self.gravity = 1
-        self.velocity = 0
-        self.on_ground = True
 
-
-    
-    def update(self):
-        keys = pg.key.get_pressed()
-        if keys[pg.K_SPACE] and self.on_ground:
-            self.velocity = self.jump_speed
-            self.on_ground = False
-
-        self.velocity += self.gravity
-        self.rect.y += self.velocity
-
-        if self.rect.bottom >= HEIGHT:
-            self.rect.bottom = HEIGHT
-            self.velocity = 0
-            self.on_ground = True
+image_paths = [   
+        'fig/gamen1.jpg',
+        'fig/gamen2.jpg',
+        'fig/gamen3.jpg',
+        'fig/gamen4.jpg',
+        'fig/gamen5.jpg',
+        'fig/gamen6.jpg'
+    ]   #スタート画面の画像一覧
 
 class StartScreen:
-    def __init__(self):
-        self.images = [
-            pygame.image.load('gamen1.jpg'),
-            pygame.image.load('gamen2.jpg'),
-            pygame.image.load('gamen3.jpg'),
-            pygame.image.load('gamen4.jpg'),
-            pygame.image.load('gamen5.jpg'),
-            pygame.image.load('gamen6.jpg')
-        ] 
+    """
+    スタート画面を表すクラス
+    """
+    def __init__(self, image_paths):
+        """
+        現在の画像を読み込んでリストに保存しインデックスを初期化する
+        """
+        self.images = [pygame.image.load(path) for path in image_paths]
         self.current_index = 0
-    def draw(self, surface):
-        surface.blit(self.images[self.current_index], (0, 0))
+    def next_image(self):
+        """
+        次の画像に進む処理を行う
+        """
+        if self.current_index < len(self.images) - 1:
+            self.current_index += 1
+            return True
+        return False 
     
-    def next_screen(self):
-        self.current_index = (self.current_index + 1) % len(self.images)
+    def get_next_screen(self):
+        """
+        現在のインデックスにある画像を返す
+        """
+        return self.images[self.current_index]
+    
     
 
+
 def main():
-    pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1200, 700))
-    start_screen=StartScreen()
-    current_screen = start_screen
+    pg.display.set_caption("Dreamer")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    start_screen=StartScreen(image_paths)
+    running = True
     clock  = pg.time.Clock()
     beamallen = None
     #背景画像をロードして、ウインドウのサイズにリサイズ
-    back_img = pg.image.load("fig/24535830.jpg") 
-    back_img = pg.transform.scale(back_img, (1200, 700))
-    allen = Allen((100, 600))
+    #allen = Allen((100, 600))
     show_allen = True
     tmr = 0
+    current_image = start_screen.get_next_screen()
+    screen.blit(current_image, (WIDTH // 2 - current_image.get_width() // 2, HEIGHT // 2 - current_image.get_height() // 2))
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 pg.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    if isinstance(current_screen, StartScreen):
-                        start_screen.next_screen()
+                if event.key == pygame.K_RETURN: #エンターキーが押されたら次の画像に進む
+                    if not start_screen.next_image():  # 次の画像に進めなかった場合
+                        running = False 
             elif event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
                 show_allen = not show_allen #アレンの表示非表示の切替(キャラの切り替えで使うかも)
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                # スペースキー押下でBeamクラスのインスタンス生成
-                beamallen = BeamAllen(allen)
         
-        current_screen.draw(screen)
-
-        screen.blit(back_img, [0, 0]) #背景画像を表すsurfase
-       
+        
+        current_image = start_screen.get_next_screen()
+        screen.blit(current_image, (WIDTH // 2 - current_image.get_width() // 2, HEIGHT // 2 - current_image.get_height() // 2)) #現在の画像を画面の中心に描画する
         key_lst = pg.key.get_pressed()
-        if show_allen:
-            allen.update(key_lst, screen)
-        if beamallen is not None:
-            beamallen.update(screen)
 
         pg.display.update()
         clock.tick(60)
